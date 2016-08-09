3ad6a94e4f5e4c51e7c8a4c9ba4c6072d00dc60e
James Spencer <thisjustosay@gmail.com>
2016-08-08 23:01:55 -0700

    normal function param style

--- www/cblite.js
+++ www/cblite.js
@@ -1,12 +1,26 @@
 function getServerURL(success, failure) {
-    cordova.exec(function(url){success(url); }, function(err) { failure(err); }, "CBLite", "getURL", []);
+    cordova.exec(
+        function (url) {
+            success(url);
+        }, function (err) {
+            failure(err);
+        }, "CBLite", "getURL", []);
 }
 
-function stopReplication(dbName,success, failure) {
-    cordova.exec(function(url){success(url); }, function(err) { failure(err); }, "CBLite", "stopReplication", [dbName]);
+function stopReplication(dbName, success, failure) {
+    cordova.exec(
+        function (url) {
+            success(url);
+        },
+        function (err) {
+            failure(err);
+        },
+        "CBLite",
+        "stopReplication",
+        [dbName]);
 }
 
 module.exports = {
-    getServerURL:getServerURL,
-    stopReplication:stopReplication
+    getServerURL: getServerURL,
+    stopReplication: stopReplication
 };
